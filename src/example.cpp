6110d51ce395bc75245d7151e6b31c2f597c0847
Ananyo Sen <ananyo@iitb.ac.in>
2019-03-17 16:24:36 +0530

    show opencl devices

--- src/example.cpp
+++ src/example.cpp
@@ -1,6 +1,22 @@
 #include <example.hpp>
+#include <iostream>
+#include <vector>
+
 
 int main()
 {
+    std::vector<cl::Platform> platforms;
+
+    cl::Platform::get(&platforms);
+    for(auto pl : platforms)
+    {
+        std::vector<cl::Device> devices;
+        std::cout << pl.getInfo<CL_PLATFORM_NAME>() << "\n";
+        pl.getDevices(CL_DEVICE_TYPE_GPU, &devices);
+        for(auto dev : devices)
+        {
+            std::cout << dev.getInfo<CL_DEVICE_NAME>() << "\n";
+        }
+    }
     return 0;
 }
